60afb5a8c3c1b69004b9a3dd26a126093f9a0b2a
Nicolas Fricke <mail@nicolasfricke.de>
2015-11-28 21:18:26 +0100

    Refactor and modulize service

--- server.py
+++ server.py
@@ -4,21 +4,37 @@ import json
 import datetime
 import yaml
 
-with open('config.yml', 'r') as f:
-  config = yaml.safe_load(f.read())
+def main():
+  config = load_config()
 
-dev = evdev.InputDevice(config['device_path'])
-print('Capturing device: ' + str(dev))
+  dev = evdev.InputDevice(config['device_path'])
+  output_line('Initialized - Capturing device: ' + str(dev))
 
-for event in dev.read_loop():
-  if event.type == evdev.ecodes.EV_KEY:
-    event = evdev.categorize(event)
-    print(str(datetime.datetime.now()) + ' - ' + str(event))
-    payload = {
-      'code': event.scancode,
-      'key': event.keycode[0] if type(event.keycode) == list else event.keycode,
-      'state': {0: 'up', 1: 'down', 2: 'hold'}[event.keystate]
-    }
-    print(payload)
-    r = requests.post(config['post_url'], json.dumps(payload))
-    print(str(datetime.datetime.now()) + ' - ' + str(r))
+  for event in dev.read_loop():
+    if event.type == evdev.ecodes.EV_KEY:
+      event = evdev.categorize(event)
+      output_line(event)
+      payload = build_payload(event)
+      output_line('Sending ' + str(payload) + ' to ' + config['post_url'])
+      response = requests.post(config['post_url'], json.dumps(payload))
+      output_line(response)
+
+def build_payload(event):
+  return {
+    'code': event.scancode,
+    'key': event.keycode[0] if type(event.keycode) == list else event.keycode,
+    'state': {0: 'UP', 1: 'DOWN', 2: 'HOLD'}[event.keystate]
+  }
+
+def load_config():
+  with open('config.yml', 'r') as f:
+    return yaml.safe_load(f.read())
+
+def timestamp_s():
+  return '[' + str(datetime.datetime.now()) + ']'
+
+def output_line(string):
+  print(timestamp_s() + ' ' + str(string))
+
+if __name__ == '__main__':
+  main()
