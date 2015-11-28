52d6d74692b16a92e59abe2d04ecf9068df856ca
Nicolas Fricke <mail@nicolasfricke.de>
2015-11-28 19:11:36 +0100

    Add POST capability, send keypresses to test server
    Uses the online service RequestBin (http://requestb.in/) for testing.
    TODO (amongst other things): extract the URL and device path to config file.

--- server.py
+++ server.py
@@ -1,10 +1,24 @@
 import evdev
+import requests
+import json
+import datetime
 
+# TODO: These should go into a configuration file
 DEVICE_PATH = '/dev/input/event0'
+URL = 'http://requestb.in/1b2bo6r1'
 
 dev = evdev.InputDevice(DEVICE_PATH)
 print('Capturing device: ' + str(dev))
 
 for event in dev.read_loop():
   if event.type == evdev.ecodes.EV_KEY:
-    print(evdev.categorize(event))
+    event = evdev.categorize(event)
+    print(str(datetime.datetime.now()) + ' - ' + str(event))
+    payload = {
+      'code': event.scancode,
+      'key': event.keycode[0] if type(event.keycode) == list else event.keycode,
+      'state': {0: 'up', 1: 'down', 2: 'hold'}[event.keystate]
+    }
+    print(payload)
+    r = requests.post(URL, json.dumps(payload))
+    print(str(datetime.datetime.now()) + ' - ' + str(r))
